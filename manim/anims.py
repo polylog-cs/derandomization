924733461e91071f89c679e0d746f044227b43e4
vaclav rozhon <vaclavrozhon@gmail.com>
2024-04-27 15:03:56 +0200

    polynomials

--- manim/anims.py
+++ manim/anims.py
@@ -114,3 +114,129 @@ class TheoremStatement(Scene):
         self.wait()
         self.play(hypothesis.animate.become(hypothesis_original))
         self.wait()
+
+
+fs = 50
+class PolynomialsIntro(Scene):
+    def construct(self):
+        set_default_colors()
+
+        polynomials = Group(*[
+            Tex(str, color=text_color, font_size=fs)
+            for str in [
+                r"{{$(x-2)^3$}}{{$ + $}}{{$x(x-2)$}}",
+                r"{{$(x+4)(x+2)(x-1)$}}{{$ - $}}{{$10x^2 + 8x$}}",
+            ]
+        ]).arrange(DOWN, buff = 1)
+
+        self.play(
+            AnimationGroup(
+                *[FadeIn(p) for p in polynomials],
+                lag_ratio=0.5,
+            )
+        )
+        self.wait()
+
+        p1 = [
+            Tex(str, color=text_color, font_size=fs).move_to(polynomials[0])
+            for str in [
+                r"{{$x^3 - 6x^2 + 12x - 8$}}{{$ + $}}{{$x(x-2)$}}",
+                r"{{$x^3 - 6x^2 + 12x - 8$}}{{$ + $}}{{$x^2 - 2x$}}",
+                r"{{$x^3 - 5x^2 + 10x - 8$}}",
+            ]
+        ]
+
+        p2 = [
+            Tex(str, color=text_color, font_size=fs).move_to(polynomials[1])
+            for str in [
+                r"{{$(x^2 + 6x + 8)(x-1)$}}{{$ - $}}{{$10x^2 + 8x$}}",
+                r"{{$x^3 + 5x^2 + 2x - 8$}}{{$ - $}}{{$10x^2 + 8x$}}",
+                r"{{$x^3 - 5x^2 + 10x - 8$}}",
+            ]
+        ]
+
+        for i, p in enumerate([p1, p2]):
+            for j in range(3):
+                self.play(
+                    Transform(polynomials[i], p[j]),
+                )
+                self.wait(0.3)
+            self.wait()
+
+        same = Tex("Same polynomial!", color = GREEN, font_size = fs).next_to(polynomials, RIGHT, buff = 1)
+        self.play(Write(same))
+        self.wait()
+        self.play(
+            *[FadeOut(o) for o in self.mobjects],
+        )
+        self.wait()
+        
+class PolynomialsIntro2(Scene):
+    def construct(self):
+        set_default_colors()
+
+        polynomials = Group(*[
+            Tex(str, color=text_color, font_size=fs)
+            for str in [
+                r"{{$($}}{{$x$}}{{$+1)^{10\,000}($}}{{$x$}}{{$+2)^{20\,000}$}}{{$\;$}}",
+                r"{{$($}}{{$x$}}{{${}^3 + 5$}}{{$x$}}{{${}^2 + 8$}}{{$x$}}{{$ + 5)^{10000}$}}",
+            ]
+        ]).arrange(DOWN, buff = 1)
+        same_poly = Tex(r"{{$(x^3 + 5x^2 + 8x + 4)^{10000}$}}", color=text_color, font_size=fs).move_to(polynomials[1])
+
+        self.play(
+            AnimationGroup(
+                *[FadeIn(p) for p in polynomials],
+                lag_ratio=0.5,
+            )
+        )
+        self.wait()
+        
+
+        # animation for generating a random integer
+        test_int = 42987
+        len = 10
+        random_ints = [random.randint(0, 100000) for _ in range(len)]
+        random_ints_tex = [Tex(str(i), color=text_color, font_size=fs).move_to(polynomials[0].get_center()).shift(2*UP) for i in random_ints + [test_int]]
+
+        run_time = 0.2
+        for i in range(len + 1):
+            self.add_sound(random_click_file())
+            if i == 0:
+                self.play(
+                    FadeIn(random_ints_tex[0]),
+                    run_time = run_time
+                )
+            else:
+                self.play(
+                    Transform(random_ints_tex[0], random_ints_tex[i]),
+                    run_time = run_time
+                )
+
+        self.wait()
+
+        p1 = [
+            Tex(str, color=text_color, font_size=fs).move_to(polynomials[0])
+            for str in [
+                r"{{$($}}{{$42987$}}{{$+1)^{10\,000}($}}{{$42987$}}{{$+2)^{20\,000}$}}{{$\;$}}",
+                r"{{$($}}{{$42987$}}{{$+1)^{10\,000}($}}{{$42987$}}{{$+2)^{20\,000}$}}$\;\;\;\; \mod 10^9+7$",
+            ]
+        ]
+
+        self.play(
+            Transform(polynomials[0], p1[0]),
+        )
+        self.play(
+            Transform(polynomials[0], p1[1]),
+        )
+
+        p2 = [
+            Tex(str, color=text_color, font_size=fs).move_to(polynomials[1])
+            for str in [
+                r"{{$(x^2 + 6x + 8)(x-1)$}}{{$ - $}}{{$10x^2 + 8x$}}",
+                r"{{$x^3 + 5x^2 + 2x - 8$}}{{$ - $}}{{$10x^2 + 8x$}}",
+                r"{{$x^3 - 5x^2 + 10x - 8$}}",
+            ]
+        ]
+
+        self.wait(10)
